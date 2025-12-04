18efb731fa6c80f9f717d6ddb2683f8a7067c9c2
Yikai Zeng <yikai.zeng@tu-dresden.de>
2025-12-04 10:31:31 +0100

    Adjust artificial delay parameter for front car movement in KraussSpeedController

--- scripts/krauss_spd_node.py
+++ scripts/krauss_spd_node.py
@@ -64,7 +64,7 @@ class KraussSpeedController(object):
         self.turn_counter = 0
         self.allow_catch_up = False
 
-        self.artificial_delay = rospy.get_param("~artificial_delay", 2.0)  # seconds
+        self.artificial_delay = rospy.get_param("~artificial_delay", 1.5)  # seconds
 
         self.is_turning = False
 
