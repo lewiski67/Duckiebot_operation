a576c3a4e38561aa1399c93b0506f6477ddbbf06
Yikai Zeng <yikai.zeng@tu-dresden.de>
2026-03-03 14:31:36 +0100

    Add ToF sensor integration and update line follower behavior

--- scripts/duckierace.py
+++ scripts/duckierace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image, Joy, Range
-from std_msgs.msg import Bool, Float32
+from std_msgs.msg import Bool, Float32, Int32
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge
 import cv2
@@ -33,6 +33,18 @@ class LineFollower:
         self.red_line_last_seen = rospy.Time.now()
 
         self.h_row_ratio = 0.6
+        self.stop_in_fuel = False
+        
+        
+        self.z_min = 0
+        self.z_max = 1.5
+        self.tof_status = 0 # For start
+        self.tof_status_sub = rospy.Subscriber("front_range_status", Int32, self.status_tof_callback, queue_size=1)
+
+        self.tof_sub = rospy.Subscriber("front_range", Range, self.tof_callback, queue_size=1)
+
+        self.tof_warn = False
+
 
         self.joy_sub    = rospy.Subscriber('joy', Joy, self.joy_callback)
         self.image_sub  = rospy.Subscriber('robot_cam/image_raw', Image, self.image_callback)
@@ -47,7 +59,7 @@ class LineFollower:
         self.power_level            = 75.0  # start at 100%
         self.power_depletion_rate   = 3    # base depletion factor
         self.low_power_threshold    = 10.0   # percent
-
+        
         self.in_fuel_zone           = False
         self.charging = False
         self.last_charge_time       = time.time()
@@ -60,11 +72,21 @@ class LineFollower:
         self.spd_pub = rospy.Publisher("speed_percent", Float32, queue_size=1)  
         fuel_topic = "in_fuel_zone"
         self.fuel_sub = rospy.Subscriber(fuel_topic, Bool, self.fuel_callback)
-        self.stop_in_fuel = False
+        
         self.power_pub.publish(data=self.power_level)
         self.fuel_timer = rospy.Timer(rospy.Duration(1.0), self.fuel_timer_callback)
         rospy.loginfo("Line follower node initialized")
+    
+    def status_tof_callback(self, msg: Int32):
+        self.tof_status = msg.data    
 
+    def tof_callback(self, msg: Range):
+        if self.tof_status == 9:
+            self.tof_range = np.clip(msg.range-0.04, self.z_min, self.z_max)  # small seeting because of mounting offset
+        else:
+            self.tof_range = self.z_max + 0.1  # invalid reading
+
+        
     def fuel_timer_callback(self, event):
         if self.charging:
             print(f"[{self.robot_name}]: Charging in fuel zone")
@@ -83,30 +105,44 @@ class LineFollower:
         now = time.time()
         if now - self.last_joy_time < 0.3:
             return
+        
+        self.last_joy_time = now
 
         if msg.buttons[4]:  # L1 to increase speed
             self.speed = min(self.speed + self.speed_step, self.max_speed)
             rospy.loginfo(f"Speed increased to: {self.speed:.2f}")
-            self.last_joy_time = now
             self.power_depletion_rate += 0.5
 
         if msg.buttons[5]:  # R1 to decrease speed
             self.speed = max(self.speed - self.speed_step, self.min_speed)
             rospy.loginfo(f"Speed decreased to: {self.speed:.2f}")
-            self.last_joy_time = now
             self.power_depletion_rate -= 0.5
 
         self.spd_pub.publish(Float32(data=(self.speed)))
         # Manual brake release (X = button[2]) yes is x
         if msg.buttons[2] and not self.last_brake_sent:
-            rospy.loginfo(f"Sending manual brake unlock to /{self.robot_name}/local_brake")
-            self.brake_pub.publish(Bool(data=False))
-            self.last_brake_sent = True
+            if not self.in_fuel_zone:
+                # you may unlock brake outside fuel zone without problem
+                rospy.loginfo(f"Sending manual brake unlock to /{self.robot_name}/local_brake")
+                self.brake_pub.publish(Bool(data=False))
+                self.last_brake_sent = True
+            else:
+                # you must holding B button to allow brake release in fuel zone, otherwise it will keep brake lock to stop you
+                if msg.buttons[1]: # B button for fuel zone brake release
+                    rospy.loginfo(f"Sending manual brake unlock to /{self.robot_name}/local_brake in fuel zone")
+                    self.brake_pub.publish(Bool(data=False))
+                    self.last_brake_sent = True
+        elif  msg.buttons[2] and self.last_brake_sent:
+            # this means you press x again while running, so we allow brake only in fuel zone
+            if self.in_fuel_zone:
+                rospy.loginfo(f"Sending manual brake lock to /{self.robot_name}/local_brake in fuel zone" )
+                self.brake_pub.publish(Bool(data=True))
+                self.last_brake_sent = False 
             
-        # elif not msg.buttons[2] and self.last_brake_sent:
+        # elif not msg.buttons[2] and self.last_brake_sent and self.in_fuel_zone: # when release brake button in fuel zone, send brake lock command to ensure stop
         #     rospy.loginfo(f"Sending manual brake lock to /{self.robot_name}/local_brake")
-        #     self.brake_pub.publish(Bool(data=False))
-        #     self.last_brake_sent = True
+        #     self.brake_pub.publish(Bool(data=True))
+        #     self.last_brake_sent = False
 
         if msg.buttons[3] and self.in_fuel_zone: # Y
             self.charging = True
@@ -114,18 +150,32 @@ class LineFollower:
             self.charging = False
 
         # Button B (index 1): prefer yellow lines, naming has legacy problem, keep for now
-        if not self.in_fuel_zone:
-            # red mode here actually means alternative color, as in yellow in the setup
-            # this command allows changing to yellow line when not in fuel zone, and will automatically come back to red if release buttons
-            self.red_mode_active = bool(msg.buttons[1]) 
-            self.stop_in_fuel = False  # Reset stop flag when toggling color mode outside fuel zone
-        elif not bool(msg.buttons[1]): # when let go button B in fuel zone, force stop to prevent going anywhere
-            self.stop_in_fuel = True
-        else:
-            self.stop_in_fuel = False
+        self.red_mode_active = bool(msg.buttons[1]) 
+        # if not self.in_fuel_zone:
+        #     # red mode here actually means alternative color, as in yellow in the setup
+        #     # this command allows changing to yellow line when not in fuel zone, and will automatically come back to red if release buttons
+        #     self.red_mode_active = bool(msg.buttons[1]) 
+        #     self.stop_in_fuel = False  # Reset stop flag when toggling color mode outside fuel zone
+        # elif not bool(msg.buttons[1]): # when let go button B in fuel zone, force stop to prevent going anywhere
+        #     self.stop_in_fuel = True
+        #     print(f"[{self.robot_name}]: Stopping in fuel zone due to button release")
+        # else:
+        #     print(f"[{self.robot_name}]: Button B pressed in fuel zone, but stop_in_fuel is {self.stop_in_fuel}")
+        #     self.stop_in_fuel = False
 
 
     def image_callback(self, msg):
+
+        if self.tof_range < 0.1:
+            if not self.tof_warn:
+                rospy.logwarn("Obstacle detected very close! Stopping.")
+                self.tof_warn = True
+            self.publish_twist(0.0, 0.0)
+            return
+        else:
+            if self.tof_warn:
+                rospy.loginfo("Obstacle cleared. Resuming line following.")
+                self.tof_warn = False
         if self.stop_in_fuel:
             self.publish_twist(0.0, 0.0)  # Ensure we stay stopped in fuel zone
             return
